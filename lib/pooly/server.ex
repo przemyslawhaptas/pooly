61d66b51f4ca3979fba6bddeed731f684e295e21
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2016-12-28 10:45:49 +0100

    Handle consumer crash

--- lib/pooly/server.ex
+++ lib/pooly/server.ex
@@ -64,6 +64,17 @@ defmodule Pooly.Server do
     {:noreply, %{state | worker_sup: worker_sup, workers: workers}}
   end
 
+  def handle_info({:DOWN, ref, _, _, _}, state = %{monitors: monitors, workers: workers}) do
+    case :ets.match(monitors, {:"$1", ref}) do
+      [[pid]] ->
+        true = :ets.delete(monitors, pid)
+        new_state = %{state | workers: [pid | workers]}
+        {:noreply, new_state}
+      [[]] ->
+        {:noreply, state}
+    end
+  end
+
   ## Helper functions
 
   defp do_init([{:mfa, mfa} | config_tail], state) do
